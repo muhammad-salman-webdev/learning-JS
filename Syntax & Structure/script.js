c03c0538da608dd5fa43eeaa39e6b2b52314a971
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2025-03-05 16:09:33 +0500

    Topic 01

---
+++ Syntax & Structure/script.js
@@ -0,0 +1,35 @@
+// console.log("hi");
+// console.log("hi");
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+var name = "Salman";
+
+console.log(name);
+
+if (true) {
+  let name2 = "Salman2";
+  console.log(name2);
+  const abc = "ABC";
+  //   abc = "ABC";
+  console.log(abc);
+}
+const a = 0;
+console.log(a);
+console.log(Boolean(a));
+
+console.log(String(1234));
+console.log(Number("1234"));
+console.log(Array(document.querySelectorAll("li")));
+
+console.log(Object("SalmanSalmanSalmanSalmanSalman"));
+
+if (a === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+a === 0 ? console.log(true) : console.log(false);
